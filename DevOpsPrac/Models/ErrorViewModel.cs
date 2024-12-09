9c8ccd6c4cbdec050285de3f3d912cc05a1a2eef
Excallent Garawaziva <Excallent.Garawaziva@green.com.na>
2024-12-09 12:52:40 +0200

    added yml

---
+++ DevOpsPrac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOpsPrac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
